b846eb3449aceadec819af0106743474c657d3aa
zhouweisheng <zhouws.com@gmail.com>
2019-08-19 23:14:31 +0800

    editorconfig

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,30 @@
-const path = require('path');
+// 第一种配置别名的方法
+// const path = require('path');
 
-function resolve(dir) {
-    return path.join(__dirname, dir);
-}
+// function resolve(dir) {
+//     return path.join(__dirname, dir);
+// }
 
+// module.exports = {
+//     lintOnSave: true,
+//     chainWebpack: (config) => {
+//         config.resolve.alias
+//             .set('@', resolve('src'))
+//             .set('assets', resolve('src/assets'))
+//             .set('components', resolve('src/components'))
+//             .set('views', resolve('src/views'));
+//     }
+// }
+
+// 第二种配置别名的方法
 module.exports = {
-    lintOnSave: true,
-    chainWebpack: (config) => {
-        config.resolve.alias
-            .set('@', resolve('src'))
-            .set('assets', resolve('src/assets'))
-            .set('components', resolve('src/components'))
-            .set('views', resolve('src/views'));
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'components': '@/components',
+                'views': '@/views'
+            }
+        }
     }
 }
